c6b1701204fd85f3b9a789cacf948654f837c8f5
Logan Sevcik <logan@sevcik.email>
2023-10-09 15:43:47 -0700

    update some urls

--- hb_web/tools/session.py
+++ hb_web/tools/session.py
@@ -3,7 +3,6 @@ from functools import wraps
 from flask import request, redirect, g, session
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 
-from tools.logging import logger
 
 def start_session(f):
     @wraps(f)
