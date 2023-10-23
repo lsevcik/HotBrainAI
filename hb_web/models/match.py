93bfeec0b3680f7f0930f3afd314b7c5b77da104
Logan Sevcik <logan@sevcik.email>
2023-10-23 15:38:56 -0700

    create matches table

---
+++ hb_web/models/match.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, ForeignKey, Identity, Integer
+from sqlalchemy.orm import mapped_column
+from database import Base
+
+
+class Match(Base):
+    __tablename__ = "matches"
+    id = Column("id", Integer, Identity(), primary_key=True)
+    user1 = mapped_column(ForeignKey("users.id"))
+    user2 = mapped_column(ForeignKey("users.id"))
+    score = Column("score", Integer)
+
+    def __init__(self, user1=None, user2=None, score=0):
+        self.user1 = user1
+        self.user2 = user2
+        self.score = score
+
+    def __repr__(self):
+        return f"<Match {self.id!r}>"
