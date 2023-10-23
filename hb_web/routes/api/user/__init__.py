3934a97e17bfb38b1d3b6c9c47be128d2671bd8b
Logan Sevcik <logan@sevcik.email>
2023-10-23 15:07:51 -0700

    Show username and allow user to logout

--- hb_web/routes/api/user/__init__.py
+++ hb_web/routes/api/user/__init__.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, session
 
 
 api_user = Blueprint("api_user", __name__)
+
+@api_user.route("/logout")
+def handle_logout():
+    session.clear()
+    return redirect(url_for("index"))
