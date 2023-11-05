963fc850a8c4f25b10f729d0e7c1ade4d2a1260b
chris <cmead87@gmail.com>
2023-11-05 09:20:09 -0800

    route added to new webpage

--- hb_web/app.py
+++ hb_web/app.py
@@ -26,6 +26,9 @@ app.logger.setLevel(10)  # DEBUG
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/login")
 def login():
