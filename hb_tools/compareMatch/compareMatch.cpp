3f2d5f0757111515d157766e90e36d8c1c619b54
ShaneDyrdahl <shane@dyrdahl.net>
2023-12-09 21:08:58 -0800

    Updated the compare algorithm to be more efficient by utilizing multithreading, specifically async. It is slightly less accurate, but it is crazy fast.

--- hb_tools/compareMatch/compareMatch.cpp
+++ hb_tools/compareMatch/compareMatch.cpp
@@ -1,15 +1,46 @@
-// Created by Shane Drydahl
-// Updated by Tucker Shaw on 11/4/2023
 #include <string>
-#include <vector>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <cmath>
-#include <cctype>
-#include <iomanip>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <future>
+#include <thread>
+
 using namespace std;
 namespace fs = filesystem;
+
+int numFiles = 0;  // DEBUG
+
+const auto processor_count = std::thread::hardware_concurrency();  // Get number of cores for current CPU
+
+// Used to ensure synchronization between processing threads
+class Semaphore {
+public:
+    explicit Semaphore(int count) : count_(count) {}
+
+    inline void notify() {
+        std::unique_lock<std::mutex> lock(mtx_);
+        count_++;
+        cv_.notify_one();
+    }
+
+    inline void wait() {
+        std::unique_lock<std::mutex> lock(mtx_);
+        while(count_ == 0) {
+            cv_.wait(lock);
+        }
+        count_--;
+    }
+
+private:
+    std::mutex mtx_;
+    std::condition_variable cv_;
+    int count_;
+};
 
 /**
  * @brief Struct containing user info
@@ -43,9 +74,9 @@ vector<user> allUserMatches; // Global vector for all user matches in process_sc
  * T -> Trad
  * M -> Male
  * F -> Female
- * 
- * @param vid 
- * @return string 
+ *
+ * @param vid
+ * @return string
  */
 string targetFolder(char vid) {
     string path0 = "standard";
@@ -66,38 +97,38 @@ string targetFolder(char vid) {
 }
 
 /**
- * @brief Checks allMatches vector for the current processed user in case their match already exists 
+ * @brief Checks allMatches vector for the current processed user in case their match already exists
  *        If it finds an existing match it will update the average and return true (false if not)
- * 
- * @param compUser 
- * @param it1 
+ *
+ * @param compUser
+ * @param it1
  * @param avg
  * @return bool
  */
-bool find_matchUser_ID(string compUser, float avg, vector<user>::iterator it1)
+bool find_matchUser_ID(const string& compUser, float avg, vector<user>::iterator it1)
 {
     vector<userMatch>::iterator it2;
     bool flag = false;
 
     for(it2 = it1->allMatches.begin(); it2 != it1->allMatches.end() && !flag; it2++)
+    {
+        if(it2->userMatchID == compUser)
         {
-            if(it2->userMatchID == compUser)
-            {
-                it2->matchAvg = (it2->matchAvg + avg) / 2;
-                flag = true;
-            }
+            it2->matchAvg = (it2->matchAvg + avg) / 2;
+            flag = true;
         }
+    }
 
     return flag;
 }
 
 /**
  * @brief Checks allUserMatches vector for the current processed user in case they already exist
- * 
- * @param procUser 
- * @return vector<user>::iterator 
+ *
+ * @param procUser
+ * @return vector<user>::iterator
  */
-vector<user>::iterator find_user_ID(string procUser)
+vector<user>::iterator find_user_ID(const string& procUser)
 {
     vector<user>::iterator it; // Create an iterator
 
@@ -105,151 +136,161 @@ vector<user>::iterator find_user_ID(string procUser)
     for(it = allUserMatches.begin(); it != allUserMatches.end(); it++)
         if(it->userID == procUser)
             return it;
-    
+
     return it; // No match is found, returns the end of the vector
 }
 
 /**
  * @brief Displays all the matches that were processed
- * 
+ *
  */
 void viewAllMatches()
 {
-    // std::cout << "Processed User ID | Compared User ID | Average" << endl; // Table header (DEBUG)
+//     std::cout << "Processed User ID | Compared User ID | Average" << endl; // Table header (DEBUG)
 
     ofstream fout;
     fout.open("output.txt");
 
     // Loop through all processed users and display their matched users and averages
-    for(vector<user>::iterator it1 = allUserMatches.begin(); it1 != allUserMatches.end(); it1++)
+    for(auto &iter : allUserMatches)
     {
-        for(vector<userMatch>::iterator it2 = it1->allMatches.begin(); it2 != it1->allMatches.end(); it2++)
+        for(auto it2 = iter.allMatches.begin(); it2 != iter.allMatches.end(); it2++)
         {
-            fout << it1->userID << ' ' << it2->userMatchID << ' ' << it2->matchAvg << endl; // Display info
+            fout << iter.userID << ' ' << it2->userMatchID << ' ' << it2->matchAvg << endl; // Display info
         }
     }
+    fout.close();
 }
 
 /**
  * @brief Stores each match into the allUserMatches vector (Will eventually be the database)
- * 
- * @param procUser 
- * @param compUser 
- * @param average 
+ *
+ * @param procUser
+ * @param compUser
+ * @param average
  */
-void storeMatches(string procUser, string compUser, float average){
-    
+void storeMatches(const string& procUser, const string& compUser, float average){
     bool flag;
+//    cout << procUser << " | " << compUser << " | AVERAGE: " << average << "\n";  // DEBUG
 
     // Store the match if needed in a vector
     vector<userMatch> currentMatch;
     currentMatch.push_back({compUser, average});
 
-    vector<user>::iterator it = find_user_ID(procUser); // Check for user in set
+    auto it = find_user_ID(procUser); // Check for user in set
 
     if(it != allUserMatches.end()) // User already in set
     {
         flag = find_matchUser_ID(compUser, average, it);
-        
+
         if(!flag)
         {
             it->allMatches.push_back({compUser, average}); // Add match to it's set
             it->size++;
         }
-    } 
+    }
     else // User not in set
         allUserMatches.push_back({procUser, currentMatch, 1}); // Add user and it's match to the set, also update the size
+
+}
+
+
+double parseDoubleFromString(const string& str, int start, int length) {
+    double result = 0.0;
+    bool negative = false;
+    bool decimalFound = false;
+    double fractionFactor = 1.0;
+    vector<double> num;
+
+    for (int i = start; i < start + length; ++i) {
+        char c = str[i];
+        if (c == '-') {
+            negative = true;
+        } else if (c == '.') {
+            decimalFound = true;
+        } else if (c >= '0' && c <= '9') {
+            if (decimalFound) {
+                fractionFactor *= 0.1;
+                result += (c - '0') * fractionFactor;
+            } else {
+                result = result * 10.0 + (c - '0');
+            }
+        }
+    }
+    return negative ? -result : result;
+}
+
+double calcRow(const string& L1, const string& L2, const vector<int>& com1, const vector<int>& com2) {
+    double sum = 0.0;
+    double LM1, LM2;
+    for (int i = 0; i < 4; i++) {
+        int start1 = com1[i] + ((i == 0) ? 0 : 1);
+        int length1 = com1[i + 1] - start1;
+        LM1 = parseDoubleFromString(L1, start1, length1);
+
+        int start2 = com2[i] + ((i == 0) ? 0 : 1);
+        int length2 = com2[i + 1] - start2;
+        LM2 = parseDoubleFromString(L2, start2, length2);
+
+        double diff = LM1 - LM2;
+        sum += diff * diff;  // Directly squaring the difference
+    }
+    return sum;
 }
 
 /**
  * @brief Comparison Algorithm: Processes the file in process_scans and user_scans to create the average
- * 
- * @param processFile 
- * @param compareFile 
+ *
+ * @param processFile
+ * @param compareFile
  */
-void processFile(fs::path processFile, fs::path compareFile) {
-    string line1 = "";
-    string line2 = "";
+static void processFile(const fs::path& processFile, const fs::path& compareFile) {
+    string line1;
+    string line2;
 
     fstream in1(processFile);
     fstream in2(compareFile);
 
+    double sum;
+    double sqroot;
 
-    // Maths
-    float O1_1;
-    float O1_2;
-    float O2_1;
-    float O2_2;
-    float T3_1;
-    float T3_2;
-    float T4_1;
-    float T4_2;
-
-    // Variables
-    float O1;
-    float O2;
-    float T3;
-    float T4;
-    float sum;
-    float sqroot;
-
-
-    // Vector for holding row calculations and commas
-    vector<float> rowSquares;
+    // Vector for holding row calculations
+    vector<double> rowSquares;
     vector<int> commas1;
     vector<int> commas2;
 
-
     int samples = 0;
-    if(in1.is_open() && in2.is_open()) {
+    if (in1.is_open() && in2.is_open()) {
         while (getline(in1, line1) && getline(in2, line2)) {
             samples++;
+
+            commas1.push_back(0);
+            commas2.push_back(0);
             for (int i = 0; i < line1.length(); i++) {
                 if (line1.at(i) == ',') {
                     commas1.push_back(i);
                 }
             }
             for (int i = 0; i < line2.length(); i++) {
-                if (line2.at(i) == ',' || line2.at(i) == '\n') {
+                if (line2.at(i) == ',') {
                     commas2.push_back(i);
                 }
             }
+            sum = calcRow(line1, line2, commas1, commas2);
 
-            commas1.push_back(line1.length() - 1);
-            commas2.push_back(line2.length() - 1);
-
-            O1_1 = stof(string(&line1[0], &line1[commas1.at(0)]));
-            O1_2 = stof(string(&line2[0], &line2[commas2.at(0)]));
-
-            O1 = powf((O1_1 - O1_2), 2);
-
-            O2_1 = stof(string(&line1[commas1.at(0) + 1], &line1[commas1.at(1)]));
-            O2_2 = stof(string(&line2[commas2.at(0) + 1], &line2[commas2.at(1)]));
-            O2 = powf((O2_1 - O2_2), 2);
-
-            T3_1 = stof(string(&line1[commas1.at(1) + 1], &line1[commas1.at(2)]));
-            T3_2 = stof(string(&line2[commas2.at(1) + 1], &line2[commas2.at(2)]));
-            T3 = powf((T3_1 - T3_2), 2);
-
-            T4_1 = stof(string(&line1[commas1.at(2) + 1], &line1[commas1.at(3)]));
-            T4_2 = stof(string(&line2[commas2.at(2) + 1], &line2[commas2.at(3)]));
-
-            T4 = powf((T4_1 - T4_2), 2);
-
-            sum = O1 + O2 + T3 + T4;
             sqroot = sqrt(sum);
             rowSquares.push_back(sqroot);
             sqroot = 0;
-
 
             commas1.clear();
             commas2.clear();
         }
     }
+    in1.close();
+    in2.close();
 
-
-    float sqrSums = 0;
-    for (float nums: rowSquares) {
+    double sqrSums = 0;
+    for (double nums: rowSquares) {
         sqrSums += nums;
     }
 
@@ -259,70 +300,66 @@ void processFile(fs::path processFile, fs::path compareFile) {
     string tempName2 = compareFile.filename().string();
     string compareUser = tempName2.substr(0, tempName2.find_last_of('_'));
 
-    // cout << "Processed User ID: " << processUser << endl; // Debug
-    // cout << "Compared User ID: " << compareUser << endl; // Debug
+    storeMatches(processUser, compareUser, float(sqrSums / samples));
+}
 
-    storeMatches(processUser, compareUser, (sqrSums / (float) samples));
-
-    // cout << "Samples: " << samples << endl; // Debug
-    // cout << "AVERAGE = " << sqrSums / (float)samples << endl << endl; // Debug
-
+void proc(const fs::path& P1, const fs::path& P2, vector<future<void>>& futures, Semaphore& sem) {
+    for (const auto& entry: fs::directory_iterator(P1)) {
+        for (const auto &entry_2: fs::directory_iterator(P2)) {
+            sem.wait(); // Wait for available slot
+            futures.push_back(std::async(std::launch::async, [&sem](const fs::path& file1, const fs::path& file2) {
+                processFile(file1, file2);
+                sem.notify(); // Release slot
+                numFiles++;   // DEBUG
+            }, entry, entry_2));
+        }
+    }
 }
 
 int main() {
-    // Get the current path for process_scans and user_scans
+    // Timers to test efficiency
+//    typedef std::chrono::high_resolution_clock Time;              // DEBUG
+//    typedef std::chrono::milliseconds ms;                         // DEBUG
+//    typedef std::chrono::duration<float> fsec;                    // DEBUG
+//    auto t0 = Time::now();                                        // DEBUG
+
     fs::path procDir = fs::current_path();
     fs::path userScans = fs::current_path();
+    fs::path userScans_Default = fs::current_path();
 
-    // Declare variables
-    char currProcFile;
+//    string curPa[] = {"trad", "standard", "male", "female"};
+//    char currProcFile;
     string currFileName;
 
-    // Set process_scans directory name
     if (procDir.filename() != "process_scans") {
         procDir /= "process_scans";
     }
 
-    // Set user_scans directory name
     if (userScans.filename() != "user_scans") {
         userScans /= "user_scans";
+        userScans_Default /= "user_scans";
     }
 
-    // cout << "Process: " << procDir << endl; // Debug
-    // cout << "UserScan: " << userScans << endl; // Debug
+    Semaphore sem(static_cast<int>(processor_count)); // Based on the number of cores available
+    vector<future<void>> futures;
 
-    // Loop through each type of directory in process_scans (S, T, M, F) to find files needed for processing
-    for (const auto &type: fs::directory_iterator(procDir)) {
-        currFileName = type.path().string();
-        currProcFile = toupper(currFileName.at(currFileName.rfind('\\') + 1));
+    for (const auto &entry_1: fs::directory_iterator(procDir)) {
+        fs::path currentCategory = entry_1.path().filename();
+        fs::path userScansTemp = userScans / currentCategory;
+        proc(entry_1, userScansTemp, futures, sem);
+    }
 
-        // cout << "File Path:" << currFileName << endl;   // Debug
-        // cout << "File Types:" << currProcFile << endl;  // Debug
-
-        userScans /= targetFolder(currProcFile);
-        // cout << "  =====================" + userScans.string() + "===================" << endl;  // Debug
-
-        procDir /= targetFolder(currProcFile);
-        // cout << "  =====================" + procDir.string() + "===================" << endl;  // Debug
-
-        // Loop through each file in process_scans directories and user_scans directories and run the algo
-        for(const auto &entry_1: fs::directory_iterator(type))
-        {
-            for (const auto &entry_2: fs::directory_iterator(userScans)) {
-                processFile(entry_1, entry_2);
-            }
-        }
-
-        // Reset each path variable
-        while (userScans.filename() != "user_scans") {
-            userScans = userScans.parent_path();
-        }
-        while (procDir.filename() != "process_scans") {
-            procDir = procDir.parent_path();
-        }
+    // Wait for all async tasks to complete
+    for (auto &f : futures) {
+        f.get();
     }
 
     viewAllMatches(); // Display all the matches in a table
 
+//    cout << "NUM OF FILES: " << numFiles << endl;                               // DEBUG
+//    cout << "NUMBER OF THREADS AVAIL: " << processor_count << endl;             // DEBUG
+//    auto t1 = Time::now();                                                      // DEBUG
+//    fsec fs = t1 - t0;                                                          // DEBUG
+//    std::cout << fs.count() << "s\n";                                           // DEBUG
     return 0;
 }
